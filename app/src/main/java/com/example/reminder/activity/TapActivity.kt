f67de11f0d4bf0c6cf62b952c72fcde5c03ec3e9
yunhansae <hansae1231@naver.com>
2024-05-27 20:02:07 +0900

    Initial commit

--- app/src/main/java/com/example/reminder/activity/TapActivity.kt
+++ app/src/main/java/com/example/reminder/activity/TapActivity.kt
@@ -1,3 +1,125 @@
+//package com.example.reminder.activity
+//
+//import android.os.Bundle
+//import android.view.View
+//import android.view.animation.Animation
+//import android.view.animation.ScaleAnimation
+//import android.widget.ImageView
+//import android.widget.LinearLayout
+//import android.widget.TextView
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.databinding.DataBindingUtil
+//import androidx.fragment.app.Fragment
+//import com.example.reminder.R
+//import com.example.reminder.databinding.ActivityTapBinding
+//import com.example.reminder.fragment.SettingFragment
+//import com.example.reminder.fragment.ReminderFragment
+//import com.example.reminder.fragment.TipFragment
+//
+//class TapActivity : AppCompatActivity() {
+//    private lateinit var binding: ActivityTapBinding
+//    private var selectedTab = Tab.REMINDER
+//
+//    private val scaleAnimation = ScaleAnimation(
+//        0.8f,
+//        1.0f,
+//        1f,
+//        1f,
+//        Animation.RELATIVE_TO_SELF,
+//        1.0f,
+//        Animation.RELATIVE_TO_SELF,
+//        0.0f
+//    ).apply {
+//        duration = 200
+//        fillAfter = true
+//    }
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_tap)
+//
+//        // 기본 프래그먼트 설정
+//        replaceFragment(ReminderFragment())
+//        setupTabListener()
+//    }
+//
+//    private fun setupTabListener() {
+//        binding.reminderLayout.setOnClickListener { selectTab(Tab.REMINDER) }
+//        binding.tipLayout.setOnClickListener { selectTab(Tab.TIP) }
+//        binding.dashboardLayout.setOnClickListener { selectTab(Tab.SETTINGS) }
+//    }
+//
+//    private fun selectTab(tab: Tab) {
+//        if (selectedTab != tab) {
+//            when (tab) {
+//                Tab.REMINDER -> {
+//                    replaceFragment(ReminderFragment())
+//                    binding.reminderLayout.startAnimation(scaleAnimation)
+//                    updateTabVisuals(binding.reminderLayout, null, null, null)
+//                }
+//
+//                Tab.TIP -> {
+//                    replaceFragment(TipFragment())
+//                    binding.tipLayout.startAnimation(scaleAnimation)
+//                    updateTabVisuals(binding.tipLayout, binding.tipImg, binding.tipText, R.drawable.tips)
+//                }
+//
+//                Tab.SETTINGS -> {
+//                    replaceFragment(SettingFragment())
+//                    binding.dashboardLayout.startAnimation(scaleAnimation)
+//                    updateTabVisuals(binding.dashboardLayout, binding.dashboardImg, binding.dashboardText, R.drawable.dashboard)
+//                }
+//            }
+//            selectedTab = tab
+//        }
+//    }
+//
+//    private fun updateTabVisuals(layout: LinearLayout, imageView: ImageView?, textView:TextView?, backImg: Int?) {
+//        binding.apply {
+//            tipLayout.background = null
+//            tipImg.setBackgroundResource(R.drawable.tip_selected)
+//            tipText.visibility = View.GONE
+//            dashboardLayout.background = null
+//            dashboardImg.setBackgroundResource(R.drawable.dashboard_selected)
+//            dashboardText.visibility = View.GONE
+//
+//            when (layout) {
+//                reminderLayout -> {
+//                    reminderLayout.setBackgroundResource(R.drawable.nav_round_bg)
+//                    reminderImgSelected.visibility = View.VISIBLE
+//                    reminderImgDefault.visibility = View.GONE
+//                    reminderText.visibility = View.VISIBLE
+//                }
+//
+//                else -> {
+//                    reminderLayout.background = null
+//                    reminderImgDefault.visibility = View.VISIBLE
+//                    reminderImgSelected.visibility = View.GONE
+//                    reminderText.visibility = View.GONE
+//
+//                    layout.setBackgroundResource(R.drawable.nav_round_bg)
+//                    imageView!!.setBackgroundResource(backImg!!)
+//                    textView!!.visibility = View.VISIBLE
+//                }
+//            }
+//        }
+//    }
+//
+//    private fun replaceFragment(fragment: Fragment) {
+//        val transaction = supportFragmentManager.beginTransaction()
+//        transaction.setReorderingAllowed(true)
+//        transaction.replace(R.id.fragment_container, fragment)
+//        transaction.commit()
+//    }
+//    override fun onBackPressed() {
+//        //super.onBackPressed();
+//    }
+//
+//    enum class Tab {
+//        REMINDER, TIP, SETTINGS
+//    }
+//}
+
 package com.example.reminder.activity
 
 import android.os.Bundle
@@ -12,6 +134,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.reminder.R
 import com.example.reminder.databinding.ActivityTapBinding
+import com.example.reminder.fragment.DonateFragment
 import com.example.reminder.fragment.SettingFragment
 import com.example.reminder.fragment.ReminderFragment
 import com.example.reminder.fragment.TipFragment
@@ -46,6 +169,9 @@ class TapActivity : AppCompatActivity() {
     private fun setupTabListener() {
         binding.reminderLayout.setOnClickListener { selectTab(Tab.REMINDER) }
         binding.tipLayout.setOnClickListener { selectTab(Tab.TIP) }
+
+        binding.sellLayout.setOnClickListener { selectTab(Tab.DONATE) }
+
         binding.dashboardLayout.setOnClickListener { selectTab(Tab.SETTINGS) }
     }
 
@@ -64,6 +190,12 @@ class TapActivity : AppCompatActivity() {
                     updateTabVisuals(binding.tipLayout, binding.tipImg, binding.tipText, R.drawable.tips)
                 }
 
+                Tab.DONATE -> {
+                    replaceFragment(DonateFragment())
+                    binding.sellLayout.startAnimation(scaleAnimation)
+                    updateTabVisuals(binding.sellLayout, binding.sellImg, binding.sellText, R.drawable.donate_selected)
+                }
+
                 Tab.SETTINGS -> {
                     replaceFragment(SettingFragment())
                     binding.dashboardLayout.startAnimation(scaleAnimation)
@@ -79,6 +211,12 @@ class TapActivity : AppCompatActivity() {
             tipLayout.background = null
             tipImg.setBackgroundResource(R.drawable.tip_selected)
             tipText.visibility = View.GONE
+
+            sellLayout.background = null
+            sellImg.setBackgroundResource(R.drawable.donate_default)
+            sellText.visibility = View.GONE
+
+
             dashboardLayout.background = null
             dashboardImg.setBackgroundResource(R.drawable.dashboard_selected)
             dashboardText.visibility = View.GONE
@@ -116,6 +254,7 @@ class TapActivity : AppCompatActivity() {
     }
 
     enum class Tab {
-        REMINDER, TIP, SETTINGS
+        REMINDER, TIP, SETTINGS, DONATE
     }
 }
+
--- app/src/main/java/com/example/reminder/fragment/DonateFragment.kt
+++ app/src/main/java/com/example/reminder/fragment/DonateFragment.kt
@@ -1,3 +1,84 @@
+//package com.example.reminder.fragment
+//
+//import android.animation.ValueAnimator
+//import android.content.Intent
+//import android.graphics.Typeface
+//import android.net.Uri
+//import android.os.Bundle
+//import android.text.SpannableString
+//import android.text.Spanned
+//import android.text.style.StyleSpan
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import androidx.databinding.DataBindingUtil
+//import androidx.fragment.app.Fragment
+//import androidx.lifecycle.ViewModelProvider
+//import com.example.reminder.R
+//import com.example.reminder.databinding.FragmentDonateBinding
+//import com.example.reminder.model.ReminderViewModel
+//import com.example.reminder.model.ReminderViewModelFactory
+//import com.example.reminder.repository.MedicineRepository
+//import com.example.reminder.data.MedicineReminderDatabase
+//
+//
+//class DonateFragment : Fragment() {
+//    private lateinit var binding: FragmentDonateBinding
+//    private lateinit var donateViewModel : ReminderViewModel
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_donate, container, false)
+//        return binding.root
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        /*super.onViewCreated(view, savedInstanceState)
+//
+//        val dao = MedicineReminderDatabase.getDatabase(requireContext()).medicineReminderDao()
+//        val repository = MedicineRepository(dao)
+//
+//        donateViewModel = ViewModelProvider(requireActivity(), ReminderViewModelFactory(repository)).get(ReminderViewModel::class.java)
+//
+//        var donateMedicine : Int
+//        donateViewModel.getDonateMedicine().observe(requireActivity(), androidx.lifecycle.Observer {
+//            if(it != null) {
+//                donateMedicine = it
+//                val anim = ValueAnimator.ofInt(0, donateMedicine)
+//
+//                anim.addUpdateListener {
+//                    binding.medicineStockTxt.text = anim.animatedValue.toString()
+//                }
+//                anim.duration = 1500;
+//                anim.start();
+//            }else{
+//                binding.medicineStockTxt.text = "0"
+////                Toast.makeText(requireContext(),"Please Add Medicine",Toast.LENGTH_SHORT).show()
+//            }
+//        })
+//
+//
+//        val text =
+//            "The Best Way is - You can return the unwanted Medicines to the Pharmacy or Donate it to a Nearby NGO. (Kindly Read the Terms & Conditions)"
+//        val ss = SpannableString(text)
+//        val boldSpan = StyleSpan(Typeface.BOLD)
+//        val boldSpan1 = StyleSpan(Typeface.BOLD)
+//        ss.setSpan(boldSpan, 26, 55, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+//        ss.setSpan(boldSpan1, 75, 81, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+//        binding.donateTxt.text = ss
+//
+//        binding.medicineDonateLink.setOnClickListener {
+//            val openURL = Intent(Intent.ACTION_VIEW)
+//            openURL.data = Uri.parse("https://www.google.com/search?rlz=1C1CHBF_enIN1025IN1025&sxsrf=APwXEderQZ2DJLuJ-gxfQfn_8asx7PvRvw:1685358772615&q=near+by+government+hospital+engios&sa=X&ved=2ahUKEwjPsdfXspr_AhXoTWwGHR6_CV8Q7xYoAHoECAYQAQ&biw=1536&bih=763&dpr=1.25")
+//            startActivity(openURL)
+//        }*/
+//
+//
+//    }
+//}
+
 package com.example.reminder.fragment
 
 import android.animation.ValueAnimator
@@ -35,7 +116,7 @@ class DonateFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        /*super.onViewCreated(view, savedInstanceState)
+        super.onViewCreated(view, savedInstanceState)
 
         val dao = MedicineReminderDatabase.getDatabase(requireContext()).medicineReminderDao()
         val repository = MedicineRepository(dao)
@@ -49,31 +130,31 @@ class DonateFragment : Fragment() {
                 val anim = ValueAnimator.ofInt(0, donateMedicine)
 
                 anim.addUpdateListener {
-                    binding.medicineStockTxt.text = anim.animatedValue.toString()
+                    //binding.medicineStockTxt.text = anim.animatedValue.toString()
                 }
                 anim.duration = 1500;
                 anim.start();
             }else{
-                binding.medicineStockTxt.text = "0"
+                //binding.medicineStockTxt.text = "0"
 //                Toast.makeText(requireContext(),"Please Add Medicine",Toast.LENGTH_SHORT).show()
             }
         })
 
 
-        val text =
-            "The Best Way is - You can return the unwanted Medicines to the Pharmacy or Donate it to a Nearby NGO. (Kindly Read the Terms & Conditions)"
-        val ss = SpannableString(text)
-        val boldSpan = StyleSpan(Typeface.BOLD)
-        val boldSpan1 = StyleSpan(Typeface.BOLD)
-        ss.setSpan(boldSpan, 26, 55, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        ss.setSpan(boldSpan1, 75, 81, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        binding.donateTxt.text = ss
+//        val text =
+//            "The Best Way is - You can return the unwanted Medicines to the Pharmacy or Donate it to a Nearby NGO. (Kindly Read the Terms & Conditions)"
+//        val ss = SpannableString(text)
+//        val boldSpan = StyleSpan(Typeface.BOLD)
+//        val boldSpan1 = StyleSpan(Typeface.BOLD)
+//        ss.setSpan(boldSpan, 26, 55, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+//        ss.setSpan(boldSpan1, 75, 81, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+//        binding.donateTxt.text = ss
 
         binding.medicineDonateLink.setOnClickListener {
             val openURL = Intent(Intent.ACTION_VIEW)
-            openURL.data = Uri.parse("https://www.google.com/search?rlz=1C1CHBF_enIN1025IN1025&sxsrf=APwXEderQZ2DJLuJ-gxfQfn_8asx7PvRvw:1685358772615&q=near+by+government+hospital+engios&sa=X&ved=2ahUKEwjPsdfXspr_AhXoTWwGHR6_CV8Q7xYoAHoECAYQAQ&biw=1536&bih=763&dpr=1.25")
+            openURL.data = Uri.parse("https://ad.goodneighbors.kr/girl24_b/web/gsa/index.html?utm_source=%EA%B5%AC%EA%B8%80PC&utm_medium=cpc&utm_campaign=girl24_b&utm_content=A1&utm_term=%EA%B5%BF%EB%84%A4%EC%9D%B4%EB%B2%84%EC%8A%A4&gad_source=1")
             startActivity(openURL)
-        }*/
+        }
 
 
     }
